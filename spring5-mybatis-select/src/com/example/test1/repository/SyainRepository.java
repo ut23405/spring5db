1a989064012d89c03256590bd9dcc1ff238e24c6
ut23405 <ut23405@gmail.com>
2021-12-19 13:48:47 +0900

    MyBatis-select

--- spring5-mybatis-select/src/com/example/test1/repository/SyainRepository.java
+++ spring5-mybatis-select/src/com/example/test1/repository/SyainRepository.java
@@ -5,8 +5,8 @@ import org.apache.ibatis.annotations.Param;
 import com.example.test1.SyainEntity;
 
 public interface SyainRepository {
-	public List<SyainEntity> findAll();
-	public SyainEntity findOne(int id);
-	public List<SyainEntity> findList(
+	List<SyainEntity> findAll();
+	SyainEntity findOne(int id);
+	List<SyainEntity> findList(
 			@Param("syain") List<Integer> syain);
 }
